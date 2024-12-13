5e38d8eb7134883ac7d9a2592d3cc0b703ce9385
Probal Nath <140540378+ProbalSourav@users.noreply.github.com>
2024-12-13 17:13:30 +0530

    model of nurse

--- HMS/nurse_management/models.py
+++ HMS/nurse_management/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Nurse(models.Model):
+    nurse_id = models.AutoField(primary_key=True)
+    nurse_name = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=15, unique=True)
+    address = models.TextField()
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True, blank=True)  
+    
+    def __str__(self):
+        return f"{self.nurse_name} (ID: {self.nurse_id})"
+    
